ec7f5e4d9848a2e984583aef68068e597d1a6905
BurakErcn <ahmedburakercan@gmail.com>
2024-01-10 19:12:36 +0300

    a
    a

--- utils/data_multitrends.py
+++ utils/data_multitrends.py
@@ -11,14 +11,13 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 class ZeroShotDataset():
-    def __init__(self, data_df, img_root, gtrends, cat_dict, col_dict, fab_dict, trend_len):
+    def __init__(self, data_df, gtrends, cat_dict, col_dict, fab_dict, trend_len):
         self.data_df = data_df
         self.gtrends = gtrends
         self.cat_dict = cat_dict
         self.col_dict = col_dict
         self.fab_dict = fab_dict
         self.trend_len = trend_len
-        self.img_root = img_root
 
     def __len__(self):
         return len(self.data_df)
@@ -31,11 +30,11 @@ class ZeroShotDataset():
 
         # Get the Gtrends time series associated with each product
         # Read the images (extracted image features) as well
-        gtrends, image_features = [], []
+        gtrends = []
         img_transforms = Compose([Resize((256, 256)), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         for (idx, row) in tqdm(data.iterrows(), total=len(data), ascii=True):
-            cat, col, fab, fiq_attr, start_date, img_path = row['category'], row['color'], row['fabric'], row['extra'], \
-                row['release_date'], row['image_path']
+            cat, col, fab, fiq_attr, start_date = row['category'], row['color'], row['fabric'], row['extra'], \
+                row['release_date']
 
             # Get the gtrend signal up to the previous year (52 weeks) of the release date
             gtrend_start = start_date - pd.DateOffset(weeks=52)
@@ -48,19 +47,14 @@ class ZeroShotDataset():
             fab_gtrend = MinMaxScaler().fit_transform(fab_gtrend.reshape(-1,1)).flatten()
             multitrends =  np.vstack([cat_gtrend, col_gtrend, fab_gtrend])
 
-
-            # Read images
-            img = Image.open(os.path.join(self.img_root, img_path)).convert('RGB')
-
             # Append them to the lists
             gtrends.append(multitrends)
-            image_features.append(img_transforms(img))
 
         # Convert to numpy arrays
         gtrends = np.array(gtrends)
 
         # Remove non-numerical information
-        data.drop(['external_code', 'season', 'release_date', 'image_path'], axis=1, inplace=True)
+        data.drop(['external_code', 'season', 'release_date'], axis=1, inplace=True)
 
         # Create tensors for each part of the input/output
         item_sales, temporal_features = torch.FloatTensor(data.iloc[:, :12].values), torch.FloatTensor(
@@ -72,9 +66,8 @@ class ZeroShotDataset():
         
         categories, colors, fabrics = torch.LongTensor(categories), torch.LongTensor(colors), torch.LongTensor(fabrics)
         gtrends = torch.FloatTensor(gtrends)
-        images = torch.stack(image_features)
 
-        return TensorDataset(item_sales, categories, colors, fabrics, temporal_features, gtrends, images)
+        return TensorDataset(item_sales, categories, colors, fabrics, temporal_features, gtrends)
 
     def get_loader(self, batch_size, train=True):
         print('Starting dataset creation process...')
